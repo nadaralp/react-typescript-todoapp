438bcc66135b7d8bf5253253a51c70b37b0bde89
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 17:35:16 +0200

    another commit 5:35

--- playground-commits.js
+++ playground-commits.js
@@ -1,4 +1,5 @@
 console.log("writing this from master");
 console.log("amending the commit with this change also");
 
-console.log("trying to amend origin/master from local master")
+console.log("trying to amend origin/master from local master");
+console.log("adding another commit");
