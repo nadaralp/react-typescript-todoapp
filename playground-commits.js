ef38e1c5eef8dd4f6dad8ac1fe68960752d17375
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 17:42:15 +0200

    Cherrypick
    commit 1 before invert
    
    # Conflicts:
    #	playground-commits.js

--- playground-commits.js
+++ playground-commits.js
@@ -1,4 +1,6 @@
 console.log("writing this from master");
 console.log("amending the commit with this change also");
 
-console.log("trying to amend origin/master from local master")
+console.log("trying to amend origin/master from local master");
+
+console.log("commit 1 before invert");
