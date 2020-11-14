89b7388bc1f99c73b9036714743b6d3572dc0633
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 18:25:38 +0200

    - Feature pull amazing work

--- playground-commits.js
+++ playground-commits.js
@@ -1,10 +1,1 @@
-console.log("writing this from master");
-console.log("amending the commit with this change also");
-
-console.log("trying to amend origin/master from local master");
-
-console.log("commit 1 before invert");
-
-console.log("work from master branch");
-console.log("more work from master branch");
-console.log("more work");
+console.log('feature-pull');
