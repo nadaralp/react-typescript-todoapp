49d4f849dae336d00f76ddd5703e5953c9b8b0eb
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 17:53:06 +0200

    Revert "commit 1 before invert"
    This reverts commit 43aaf411a42c525fd43fd86325b0c75883ea8a4d.
    
    # Conflicts:
    #	playground-commits.js

--- playground-commits.js
+++ playground-commits.js
@@ -1,8 +1,4 @@
 console.log("writing this from master");
 console.log("amending the commit with this change also");
 
-console.log("trying to amend origin/master from local master");
-
-console.log("commit 1 before invert");
-console.log("commit 2 before invert");
-console.log("commit 3 before invert");
+console.log("trying to amend origin/master from local master")
