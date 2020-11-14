0200462ea09b7775a5698124bfae310c7d9b09d8
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 18:01:40 +0200

    - Added some content from feature brach

--- playground-commits.js
+++ playground-commits.js
@@ -4,3 +4,6 @@ console.log("amending the commit with this change also");
 console.log("trying to amend origin/master from local master");
 
 console.log("commit 1 before invert");
+
+console.log("from feature branch 1");
+console.log("more from feature branch 1");
