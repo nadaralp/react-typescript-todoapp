8eea6f7faab80395c6e007e94b5b7af5bdd1fdf3
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 17:42:44 +0200

    - Commit 2 + 3 before invert

--- playground-commits.js
+++ playground-commits.js
@@ -4,3 +4,5 @@ console.log("amending the commit with this change also");
 console.log("trying to amend origin/master from local master");
 
 console.log("commit 1 before invert");
+console.log("commit 2 before invert");
+console.log("commit 3 before invert");
