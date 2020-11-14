bdf1b67b749f0610112e7fd6377c12e012167015
Nadar Alpenidze <nadaralp16@gmail.com>
2020-11-14 17:35:45 +0200

    Revert "another commit 5:35"
    This reverts commit 438bcc66135b7d8bf5253253a51c70b37b0bde89.

--- playground-commits.js
+++ playground-commits.js
@@ -1,5 +1,4 @@
 console.log("writing this from master");
 console.log("amending the commit with this change also");
 
-console.log("trying to amend origin/master from local master");
-console.log("adding another commit");
+console.log("trying to amend origin/master from local master")
